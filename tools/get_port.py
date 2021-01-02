e4e88ed1db643591f155382b9213b9e05be37ed6
lbussy <lee@bussy.org>
2021-01-02 15:19:08 -0600

    Cleanup ini and move tools

---
+++ tools/get_port.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+#
+# Created by Lee Bussy on 1/2/21
+#
+
+import platform
+Import("env")
+
+system = platform.system()
+if system == "Darwin":
+    # I'll leave it up to @thorrak to figure out how to
+    # lay this out.
+    env["UPLOAD_PORT"] = "/dev/cu.usbserial-*"
+    # env["UPLOAD_PORT"] = "/dev/cu.wch*"
+elif system == "Windows":
+    # Windows users don't need no silly declarations
+    # port = "COM*"
+    # usbc = "COM*"
+    pass
+elif system == "Linux":
+    # port = "/dev/cu.usbserial-*"
+    # usbc = "/dev/cu.wch*"
+    pass
+else:
+    pass
