5864364f069557627435f86bcd4f914a4ef53efe
lbussy <lee@bussy.org>
2021-01-04 13:50:18 -0600

    Bring in better serial logging capability

---
+++ src/serialhandler.cpp
@@ -0,0 +1,233 @@
+//
+// Created by Lee Bussy on 1/4/21
+//
+
+#include "serialhandler.h"
+
+#undef SERIAL
+#if DOTELNET == true
+ESPTelnet SerialAndTelnet;
+#define SERIAL SerialAndTelnet // Use Telnet
+#else
+#define SERIAL Serial // Use hardware serial
+#endif
+
+void serial()
+{
+#if DOTELNET == true
+    char buffer[32];
+    strcpy(buffer, (const char *)"Connected to TiltBridge\n");
+    SERIAL.setWelcomeMsg(buffer);
+#endif
+    SERIAL.begin(BAUD);
+    Serial.setDebugOutput(false);
+    SERIAL.println();
+    SERIAL.flush();
+    SERIAL.setDebugOutput(true);
+    Log.begin(ARDUINO_LOG_LEVEL, &SERIAL, true);
+    Log.setPrefix(printTimestamp);
+    Log.notice(F("Serial logging started at %l." CR), BAUD);
+}
+
+void printTimestamp(Print *_logOutput)
+{
+    char c[12];
+    sprintf(c, "%10lu ", millis());
+    _logOutput->print(c);
+}
+
+size_t printDot()
+{
+    return printDot(false);
+}
+
+size_t printDot(bool safe)
+{
+#ifndef ARDUINO_LOG_LEVEL
+    return SERIAL.print(F("."));
+#else
+    return 0;
+#endif
+}
+
+size_t printChar(const char *chr)
+{
+    return printChar(false, chr);
+}
+
+size_t printChar(bool safe, const char *chr)
+{
+#ifndef ARDUINO_LOG_LEVEL
+    return SERIAL.println(chr);
+#else
+    return 0;
+#endif
+}
+
+size_t printCR()
+{
+    return printCR(false);
+}
+
+size_t printCR(bool safe)
+{
+#ifndef ARDUINO_LOG_LEVEL
+    return SERIAL.println();
+#else
+    return 0;
+#endif
+}
+
+void flush()
+{
+    flush(false);
+}
+
+void flush(bool safe)
+{
+    SERIAL.flush();
+}
+
+void serialLoop()
+{
+#if DOTELNET == true
+    SerialAndTelnet.handle();
+    if (SerialAndTelnet.available() > 0)
+    {
+#else
+    if (Serial.available() > 0)
+    {
+#endif
+        // TODO:  Can put a serial handler in here
+    }
+}
+
+size_t myPrint(const __FlashStringHelper *ifsh)
+{
+    return SERIAL.print(ifsh);
+}
+
+size_t myPrint(const String &s)
+{
+    return SERIAL.print(s);
+}
+
+size_t myPrint(const char str[])
+{
+    return SERIAL.print(str);
+}
+
+size_t myPrint(char c)
+{
+    return SERIAL.print(c);
+}
+
+size_t myPrint(unsigned char b, int base)
+{
+    return SERIAL.print(b, base);
+}
+
+size_t myPrint(int n, int base)
+{
+    return SERIAL.print(n, base);
+}
+
+size_t myPrint(unsigned int n, int base)
+{
+    return SERIAL.print(n, base);
+}
+
+size_t myPrint(long n, int base)
+{
+    return SERIAL.print(n, base);
+}
+
+size_t myPrint(unsigned long n, int base)
+{
+    return SERIAL.print(n, base);
+}
+
+size_t myPrint(double n, int digits)
+{
+    return SERIAL.print(n, digits);
+}
+
+size_t myPrint(const Printable &x)
+{
+    return SERIAL.print(x);
+}
+
+size_t myPrint(struct tm *timeinfo, const char *format)
+{
+    return SERIAL.print(timeinfo, format);
+}
+
+// size_t myPrintf(const char *format, ...)
+// {
+//     return SERIAL.printf(*format, ...);
+// }
+
+size_t myPrintln(const __FlashStringHelper *ifsh)
+{
+    return SERIAL.println(ifsh);
+}
+
+size_t myPrintln(void)
+{
+    return SERIAL.println();
+}
+
+size_t myPrintln(const String &s)
+{
+    return SERIAL.println(s);
+}
+
+size_t myPrintln(const char c[])
+{
+    return SERIAL.println(c);
+}
+
+size_t myPrintln(char c)
+{
+    return SERIAL.println(c);
+}
+
+size_t myPrintln(unsigned char b, int base)
+{
+    return SERIAL.println(b, base);
+}
+
+size_t myPrintln(int num, int base)
+{
+    return SERIAL.println(num, base);
+}
+
+size_t myPrintln(unsigned int num, int base)
+{
+    return SERIAL.println(num, base);
+}
+
+size_t myPrintln(long num, int base)
+{
+    return SERIAL.println(num, base);
+}
+
+size_t myPrintln(unsigned long num, int base)
+{
+    return SERIAL.println(num, base);
+}
+
+size_t myPrintln(double num, int digits)
+{
+    return SERIAL.println(num, digits);
+}
+
+size_t myPrintln(const Printable &x)
+{
+    return SERIAL.println(x);
+}
+
+size_t myPrintln(struct tm *timeinfo, const char *format)
+{
+    return SERIAL.println(timeinfo, format);
+}
