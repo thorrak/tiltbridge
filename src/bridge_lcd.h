81e9103c4e63140e70535fb20ff2b4927bd5276f
Tim <17855860+pletch@users.noreply.github.com>
2021-01-13 11:40:58 -0500

    Add lcd reinit func to change orientation

--- src/bridge_lcd.h
+++ src/bridge_lcd.h
@@ -61,7 +61,7 @@ public:
     bridge_lcd();
 
     void init();
-    void stop();
+    void reinit();
     void display_logo();
 
     void display_wifi_connect_screen(const char *ap_name, const char *ap_pass);
